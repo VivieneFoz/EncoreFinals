8ba4860e14134d253e62a6cc2bb54277bfd28e14
VivieneFoz <32761258+VivieneFoz@users.noreply.github.com>
2019-05-07 12:23:25 +0800

    Add files via upload

---
+++ Quizphp.php
@@ -0,0 +1,30 @@
+<doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Encore</title>
+</head>
+<body>
+<form action="" method="post">
+	<table> align="center">
+		<tr>
+			<td>What is HTTP?</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td><input type="radio" name="http" value="Hypertext Transfer Protocol"</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td><input type="radio" name="http" value="Hypertext Transmission Protocol"</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td><input type="radio" name="http" value="Hyper Text Transfer Program"</td>
+		</tr>
+		<tr>
+			<td> </td>
+			<td><input type="radio" name="http" value="Hypertext Tranmission Program"</td>
+		</tr>
+			</form>
+</body>
